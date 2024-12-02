086776595e83a73db9da78a5ded8ba5f20309c99
Toshiyuki Takahagi <tosiyuki@hh.iij4u.or.jp>
2024-12-02 17:46:06 +0900

    CopySignedFiles メソッドの処理を大幅に変更
    - 署名済みファイルの展開処理のアラート表示をコメントアウト
    - BaseFolder が空または存在しない場合のチェックを追加
    - ZIP ファイル選択用のダイアログを追加
    - ZIP ファイルの内容を基準フォルダに展開する処理を追加
      - 基準フォルダに存在する場合は上書き確認
      - 存在しない場合はサブフォルダに展開確認

--- CopyFiles/ViewModels/SelectWorkViewModel.cs
+++ CopyFiles/ViewModels/SelectWorkViewModel.cs
@@ -3,6 +3,8 @@ using CommunityToolkit.Mvvm.Input;
 using CopyFiles.Core.Models;
 using CopyFiles.Extensions.UI.Abstractions;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.IO.Compression;
 
 namespace CopyFiles.ViewModels;
 
@@ -82,9 +84,49 @@ public partial class SelectWorkViewModel : ObservableObject
 	[RelayCommand]
 	void CopySignedFiles()
 	{
-		App.DispAlert.Show( "工事中…署名済みファイルの展開処理" );
+		//App.DispAlert.Show( "工事中…署名済みファイルの展開処理" );
 		// ほかの処理みたいに確認のときに、zipファイルを参照して、それを元に選択式に展開するのがいいか？
 		// でもそのままでもいいと思うんだよねぇ…悩ましいところだけども…
+
+		if( string.IsNullOrEmpty( App.ProjectSettingManager.CurrentSetting.SignerFileSetting.BaseFolder ) ||
+			!System.IO.Directory.Exists( App.ProjectSettingManager.CurrentSetting.SignerFileSetting.BaseFolder ) )
+		{
+			App.DispAlert.Show( "先に、未署名ファイルの圧縮で、基準フォルダを設定してください" );
+			return;
+		}
+		var dlg = new Microsoft.Win32.OpenFileDialog();
+		dlg.Filter = "Zipファイル(*.zip)|*.zip";
+		if( dlg.ShowDialog( Extensions.UI.WPF.Utilities.GetOwnerWindow() ) == true )
+		{
+			// フォルダを調整しないと展開できない場合もあるから、ここは手動でやらないとダメ…だろうな。
+			//System.IO.Compression.ZipFile.ExtractToDirectory( dlg.FileName, App.ProjectSettingManager.CurrentSetting.SignerFileSetting.BaseFolder );
+			using( var archive = System.IO.Compression.ZipFile.OpenRead( dlg.FileName ) )
+			{
+				System.IO.Compression.ZipArchiveEntry firstEntry = archive.Entries.First();
+				var filePath = Path.Combine( App.ProjectSettingManager.CurrentSetting.SignerFileSetting.BaseFolder, firstEntry.FullName );
+				if( File.Exists( filePath ) )
+				{
+					// この場所であっているのでそのまま展開する
+					if( App.DispAlert.Show( "圧縮ファイルの内容で更新しますか？", IDispAlert.Buttons.YesNo, IDispAlert.Icon.Question ) == IDispAlert.Result.Yes )
+					{
+						foreach( var entry in archive.Entries )
+						{
+							filePath = Path.Combine( App.ProjectSettingManager.CurrentSetting.SignerFileSetting.BaseFolder, entry.FullName );
+							entry.ExtractToFile( filePath, true );
+						}
+					}
+				}
+				else
+				{
+					// この場所でないので、展開して手動でコピーしてもらう
+					if( App.DispAlert.Show( "圧縮ファイルを直接展開できないので、サブフォルダに展開します。", IDispAlert.Buttons.YesNo, IDispAlert.Icon.Question ) == IDispAlert.Result.Yes )
+					{
+						var extractDir = Path.Combine( Path.GetDirectoryName( dlg.FileName )!, Path.GetFileNameWithoutExtension( dlg.FileName ) );
+						archive.ExtractToDirectory( extractDir );
+					}
+				}
+			}
+		}
 	}
 
 	public SelectWorkViewModel()
