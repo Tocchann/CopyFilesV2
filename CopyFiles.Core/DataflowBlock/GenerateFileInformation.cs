ea5829f1fb24d6e6565e51cca98279aad1509172
Toshiyuki Takahagi <tosiyuki@hh.iij4u.or.jp>
2025-07-24 13:56:36 +0900

    Generateメソッドのフィルタリングとエラーハンドリング
    `GenerateFileInformation` クラスの `Generate` メソッドにおいて、`settings` コレクションからのフィルタリングを追加し、`filePath` が `setting.BaseFolder` で始まる場合のみ処理されるようにしました。`isAbsolute` が `true` の場合には `refPath` を `setting.ReferenceFolder` と結合し、ファイルの存在確認を行う処理を追加しました。また、`setting.ReferenceFolder` で始まる場合の処理も追加し、こちらでも `refPath` の計算とファイルの存在確認を行うようにしました。これにより、ファイルのパスに基づく厳密な処理と無効なパスに対するエラーハンドリングが強化されました。

--- CopyFiles.Core/DataflowBlock/GenerateFileInformation.cs
+++ CopyFiles.Core/DataflowBlock/GenerateFileInformation.cs
@@ -7,22 +7,33 @@ public class GenerateFileInformation
 {
 	public static TargetFileInformation Generate( IEnumerable<ReferFolder> settings, string filePath, bool isAbsolute )
 	{
-		foreach( var setting in settings )
+		foreach( var setting in settings.Where( s => filePath.StartsWith( s.BaseFolder ) ) )
 		{
-			if( filePath.StartsWith( setting.BaseFolder ) )
+			var refPath = Path.GetRelativePath( setting.BaseFolder, filePath );
+			if( isAbsolute )
 			{
-				var refPath = Path.GetRelativePath( setting.BaseFolder, filePath );
-				if( isAbsolute )
+				refPath = Path.Combine( setting.ReferenceFolder, refPath );
+				// 絶対パスの場合は、参照先から集めてくるので、存在するところにマッチさせる(複数設定されてるはずなので)
+				if( !File.Exists( refPath ) )
 				{
-					refPath = Path.Combine( setting.ReferenceFolder, refPath );
-					// 絶対パスの場合は、参照先から集めてくるので、存在するところにマッチさせる(複数設定されてるはずなので)
-					if( !File.Exists( refPath ) )
-					{
-						continue;
-					}
+					continue;
 				}
-				return new( new( filePath, true ), new( refPath, isAbsolute ) );
 			}
+			return new( new( filePath, true ), new( refPath, isAbsolute ) );
+		}
+		foreach( var setting in settings.Where( s => filePath.StartsWith( s.ReferenceFolder ) ) )
+		{
+			var refPath = Path.GetRelativePath( setting.ReferenceFolder, filePath );
+			if( isAbsolute )
+			{
+				refPath = Path.Combine( setting.BaseFolder, refPath );
+				// 絶対パスの場合は、参照先から集めてくるので、存在するところにマッチさせる(複数設定されてるはずなので)
+				if( !File.Exists( filePath ) )
+				{
+					continue;
+				}
+			}
+			return new( new( refPath, isAbsolute ), new( filePath, true ) );
 		}
 		throw new InvalidDataException( $"{filePath} をコピーするための設定がありません。" );
 	}
